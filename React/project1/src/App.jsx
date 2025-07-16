767c3b751c7c869595c75ee6fde682be32096ad7
Vaibhav Kumar Thakur <vaibhavkr522@gmail.com>
2025-07-16 18:19:45 +0530

    new commit

--- React/project1/src/App.jsx
+++ React/project1/src/App.jsx
@@ -1,18 +1,20 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
+      <CustomButton count={count} setCount={setCount} />
     </>
   );
+}
+
+function CustomButton(props) {
+  function onClickHandler() {
+    props.setCount(props.count + 1);
+  }
+  return <button onClick={onClickHandler}>Counter : {props.count}</button>;
 }
 
 export default App;
