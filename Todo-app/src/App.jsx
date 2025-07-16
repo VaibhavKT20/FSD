2a94b345d96521505bd5878445a22eae1b4e3add
Vaibhav Kumar Thakur <vaibhavkr522@gmail.com>
2025-07-16 20:03:00 +0530

    new commit

--- Todo-app/src/App.jsx
+++ Todo-app/src/App.jsx
@@ -4,9 +4,57 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [todo, setTodo] = useState([
+    {
+      id: 1,
+      title: "Learn React",
+      description: "Study the React documentation and build a small project.",
+      completed: false,
+    },
+    {
+      id: 2,
+      title: "Learn Vite",
+      description: "Understand the Vite build tool and its advantages.",
+      completed: false,
+    },
+    {
+      id: 3,
+      title: "Build a Todo App",
+      description: "Create a simple Todo application using React and Vite.",
+      completed: false,
+    },
+  ]);
 
-  return <></>;
+  return (
+    <div>
+      <Todo title="React" description="Learn React"></Todo>
+      <Todo title="Vite" description="Learn Vite"></Todo>
+      <Todo title="Todo App" description="Build a Todo App"></Todo>
+      {/* 
+      <Todo title={todo[0].title} description={todo[0].description}></Todo>
+      <Todo title={todo[1].title} description={todo[1].description}></Todo>
+      <Todo title={todo[2].title} description={todo[2].description}></Todo> */}
+
+      {todo.map((item) => {
+        return (
+          <Todo
+            key={item.id}
+            title={item.title}
+            description={item.description}
+          ></Todo>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
+
+function Todo(props) {
+  return (
+    <div className="todo">
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+    </div>
+  );
+}
