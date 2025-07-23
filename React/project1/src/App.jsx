137669525d038f26f3f77d01c59256ac09bbf73a
Vaibhav Kumar Thakur <vaibhavkr522@gmail.com>
2025-07-23 21:01:43 +0530

    new commit

--- React/project1/src/App.jsx
+++ React/project1/src/App.jsx
@@ -1,20 +1,40 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
+// function App() {
+//   const [count, setCount] = useState(0);
 
+//   return (
+//     <>
+//       <CustomButton count={count} setCount={setCount} />
+//     </>
+//   );
+// }
+
+// function CustomButton(props) {
+//   function onClickHandler() {
+//     props.setCount(props.count + 1);
+//   }
+//   return <button onClick={onClickHandler}>Counter : {props.count}</button>;
+// }
+
+// export default App;
+
+
+
+//Use Effect Hook Example
+
+import React from "react";
+import { useEffect } from "react";
+const App = () => {
+  useEffect(() => {
+    alert("Hii");
+  }, []);
   return (
-    <>
-      <CustomButton count={count} setCount={setCount} />
-    </>
+    <div>
+      <h1>Use Effect Hook Example</h1>
+      <p>This is a simple example of using the useEffect hook in React.</p>
+    </div>
   );
-}
-
-function CustomButton(props) {
-  function onClickHandler() {
-    props.setCount(props.count + 1);
-  }
-  return <button onClick={onClickHandler}>Counter : {props.count}</button>;
-}
+};
 
 export default App;
