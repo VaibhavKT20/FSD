c457d68dfab554e54ddbed9f1eab684fbae559f1
Vaibhav Kumar Thakur <vaibhavkr522@gmail.com>
2025-07-24 16:49:27 +0530

    new commit

--- React/project1/src/App.jsx
+++ React/project1/src/App.jsx
@@ -19,22 +19,71 @@
 
 // export default App;
 
-
-
 //Use Effect Hook Example
 
+// import React from "react";
+// import { useEffect } from "react";
+// const App = () => {
+//   useEffect(() => {
+//     alert("Hii");
+//   }, []);
+//   return (
+//     <div>
+//       <h1>Use Effect Hook Example</h1>
+//       <p>This is a simple example of using the useEffect hook in React.</p>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+//useEffect with Dependency Example
+
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+// const App = () => {
+//   const [todos, setTodos] = useState([]);
+
+//   useEffect(() => {
+//     axios
+//       .get("https://jsonplaceholder.typicode.com/todos")
+//       .then(function (response) {
+//         console.log("API Response:", response.data);
+//         setTodos(response.data);
+//       })
+//       .catch(function (error) {
+//         console.error("Error fetching todos:", error);
+//       });
+//   }, []);
+
+//   return (
+//     <div>
+//       {todos.map((todo) => (
+//         <Todo title={todo.title} description={todo.description} />
+//       ))}
+//     </div>
+//   );
+// };
+// function Todo({ title, description }) {
+//   return (
+//     <div>
+//       <h2>{title}</h2>
+//       <p>{description}</p>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+//useMemo Example
+
 import React from "react";
-import { useEffect } from "react";
-const App = () => {
-  useEffect(() => {
-    alert("Hii");
-  }, []);
+
+export const App = () => {
   return (
     <div>
-      <h1>Use Effect Hook Example</h1>
-      <p>This is a simple example of using the useEffect hook in React.</p>
+      <input type="text" />
+      <button>Counter</button>
     </div>
   );
 };
-
-export default App;
