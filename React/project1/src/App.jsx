2ada17ddb1dab218a153e601ee9c4359d69c43c5
Vaibhav Kumar Thakur <vaibhavkr522@gmail.com>
2025-07-25 12:01:16 +0530

    new commit

--- React/project1/src/App.jsx
+++ React/project1/src/App.jsx
@@ -77,13 +77,44 @@
 
 //useMemo Example
 
+// import React, { useMemo, useState } from "react";
+
+// function App() {
+//   const [counter, setCounter] = useState(0);
+//   const [inputValue, setInputValue] = useState(1);
+
+//   const count = useMemo(() => {
+//     let finalCount = 0;
+//     for (let i = 1; i <= inputValue; i++) {
+//       finalCount = finalCount + i;
+//     }
+//     return finalCount;
+//   }, [inputValue]);
+//   return (
+//     <div>
+//       <input
+//         onChange={function (e) {
+//           setInputValue(e.target.value);
+//         }}
+//         placeholder={"Find sum from 1 to n"}
+//       />
+//       <br />
+//       Sum from 1 to {inputValue} is {count}
+//       <br />
+//       <button onClick={() => setCounter(counter + 1)}>
+//         Counter ({counter})
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+//useCallback Example
 import React from "react";
 
-export const App = () => {
-  return (
-    <div>
-      <input type="text" />
-      <button>Counter</button>
-    </div>
-  );
-};
+function App() {
+  return <div>App</div>;
+}
+
+export default App;
