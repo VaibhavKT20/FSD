9eb60445625ddf839d31650968dadc5d3eea532a
Vaibhav Kumar Thakur <vaibhavkr522@gmail.com>
2025-08-15 20:04:34 +0530

    react-router-dom

---
+++ react-router-dom/src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from "react";
+import Dashboard from "../components/Dashboard";
+import Landing from "../components/landing";
+// function App() {
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <div>
+//       <div
+//         style={{
+//           background: "black",
+//           color: "white",
+//           padding: "10px",
+//           textAlign: "center",
+//         }}
+//       >
+//         <h1>React Router DOM Example</h1>
+//       </div>
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/dashboard" element={<Dashboard />} />
+//           <Route path="/" element={<Landing />} />
+//         </Routes>
+//       </BrowserRouter>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+function App() {
+  return (
+    <div>
+      <div>
+        <button
+          onClick={() => {
+            window.location.href = "/";
+          }}
+        >
+          Landing page
+        </button>
+        <button
+          onClick={() => {
+            window.location.href = "/dashboard";
+          }}
+        >
+          Dashboard
+        </button>
+      </div>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<Landing />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
