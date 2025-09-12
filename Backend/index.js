7a6dcebb0175ae0bcb133fd5f8511a1819ba9a6f
Vaibhav Kumar Thakur <vaibhavkr522@gmail.com>
2025-09-12 17:06:50 +0530

    backend code

---
+++ Backend/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/twitter", (req, res) => {
+  res.send("Twitter API endpoint");
+});
+
+app.get("/login", (req, res) => {
+  res.send("<h1>Please login to continue</h1>");
+});
+
+app.get("/signup", (req, res) => {
+  res.send("<h1>Please signup to continue</h1>");
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
