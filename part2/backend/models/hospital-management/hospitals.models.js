2c6190cae744de14a608d13600cb143a3feb3e62
Vaibhav Kumar Thakur <vaibhavkr522@gmail.com>
2025-09-15 20:42:48 +0530

    mongoose schema new commit

---
+++ part2/backend/models/hospital-management/hospitals.models.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const hospitalSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    bedCount: {
+      type: Number,
+      required: true,
+    },
+    doctors: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Doctor",
+      },
+    ],
+    patients: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Patient",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const Hospital = mongoose.model("Hospital", hospitalSchema);
---
+++ part2/backend/models/hospital-management/medical_record.models.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const medicalRecordSchema = new mongoose.Schema(
+  {
+    patient: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Patient",
+      required: true,
+    },
+    doctor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Doctor",
+      required: true,
+    },
+    diagnosis: {
+      type: String,
+      required: true,
+    },
+    treatment: {
+      type: String,
+      required: true,
+    },
+    dateOfVisit: {
+      type: Date,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+export const MedicalRecord = mongoose.model(
+  "MedicalRecord",
+  medicalRecordSchema
+);
